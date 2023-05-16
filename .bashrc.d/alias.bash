659390f31bbc96375433f9bd047f3825f080d57a
William <weilin.su@gmail.com>
2023-05-16 22:58:53 +0800

    Update
    - add more hosts in ssh config
    - add git push command for dotfiles

--- .bashrc.d/alias.bash
+++ .bashrc.d/alias.bash
@@ -1,1 +1,3 @@
 alias dotfiles_git='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfiles_git_push='dotfiles_git push --set-upstream origin main'
+alias push_dotfiles='dotfiles_git push --set-upstream origin main'
