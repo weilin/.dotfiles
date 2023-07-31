d84be2ffe4861dadc421374d9ef85de125902f6f
William <weilin.su@gmail.com>
2023-07-31 13:31:03 +0800

    dotfiles git: test if bash-complelete for git is defned

--- .bashrc.d/dotfiles_git.bash
+++ .bashrc.d/dotfiles_git.bash
@@ -5,9 +5,18 @@ alias dotfiles_git='git-dotfiles'
 
 alias git-dotfiles-push='git-dotfiles push --set-upstream origin main'
 
-if [ -e $GIT_COMP_FILE ]
+
+if ! [[ $(type -t ___git_complete) == function ]]
 then
-	source $GIT_COMP_FILE
+	if [ -e $GIT_COMP_FILE ]
+	then
+		source $GIT_COMP_FILE
+	fi
+fi
+
+
+if [[ $(type -t ___git_complete) == function ]]
+then
 	___git_complete git-dotfiles __git_main
 	___git_complete dotfiles_git __git_main
 fi
